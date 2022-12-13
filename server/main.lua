5a0539968e0352ddff69c13f234084544cf04b19
bitpredator <67551273+bitpredator@users.noreply.github.com>
2022-12-13 18:50:06 +0100

    refactor:(code reconstruction with context implementation)

--- server/main.lua
+++ server/main.lua
@@ -9,46 +9,10 @@ TriggerEvent('esx_society:registerSociety', 'import', 'Import', 'society_import'
     type = 'public'
 })
 
-RegisterNetEvent('bpt_importjob:success')
-AddEventHandler('bpt_importjob:success', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local timeNow = os.clock()
-
-    if xPlayer.job.name == 'import' then
-        if not lastPlayerSuccess[source] or timeNow - lastPlayerSuccess[source] > 5 then
-            lastPlayerSuccess[source] = timeNow
-
-            math.randomseed(os.time())
-            local total = math.random(Config.NPCJobEarnings.min, Config.NPCJobEarnings.max)
-
-            if xPlayer.job.grade >= 3 then
-                total = total * 2
-            end
-
-            TriggerEvent('esx_addonaccount:getSharedAccount', 'society_import', function(account)
-                if account then
-                    local playerMoney = ESX.Math.Round(total / 100 * 30)
-                    local societyMoney = ESX.Math.Round(total / 100 * 70)
-
-                    xPlayer.addMoney(playerMoney, "Import Fair")
-                    account.addMoney(societyMoney)
-
-                    xPlayer.showNotification(_U('comp_earned', societyMoney, playerMoney))
-                else
-                    xPlayer.addMoney(total, "Import Fair")
-                    xPlayer.showNotification(_U('have_earned', total))
-                end
-            end)
-        end
-    else
-        print(('[^3WARNING^7] Player ^5%s^7 attempted to ^5bpt_importjob:success^7 (cheating)'):format(source))
-    end
-end)
-
 ESX.RegisterServerCallback("bpt_importjob:SpawnVehicle", function(source, cb, model , props)
     local xPlayer = ESX.GetPlayerFromId(source)
 
-    if xPlayer.job.name ~= "import" then 
+    if xPlayer.job.name ~= "import" then
         print(('[^3WARNING^7] Player ^5%s^7 attempted to Exploit Vehicle Spawing!!'):format(source))
         return
     end
